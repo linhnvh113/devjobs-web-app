4ba9f577a4ef7088590f9f5d1a5c42ea8d977e3b
linhnvh113 <linhnvh113@gmail.com>
2024-03-31 20:43:08 +0700

    Added assets files

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,23 +1,28 @@
-import type { Config } from "tailwindcss"
+import type { Config } from "tailwindcss";
 
 const config = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-	],
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
   prefix: "",
   theme: {
     container: {
       center: true,
-      padding: "2rem",
+      padding: "1.5rem",
       screens: {
-        "2xl": "1400px",
+        xl: "1100px",
       },
     },
     extend: {
+      backgroundImage: {
+        "header-desktop": "url('/desktop/bg-pattern-header.svg')",
+        "header-tablet": "url('/tablet/bg-pattern-header.svg')",
+        "header-mobile": "url('/mobile/bg-pattern-header.svg')",
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
@@ -75,6 +80,6 @@ const config = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-} satisfies Config
+} satisfies Config;
 
-export default config
+export default config;
